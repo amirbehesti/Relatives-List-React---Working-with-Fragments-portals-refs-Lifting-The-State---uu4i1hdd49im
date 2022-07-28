71e6a48bb67cb96b18856e935ada303efd597e71
amirbehesti <amirbehesti392@gmail.com>
2022-07-28 18:00:29 +0530

    third

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+const relativeList = ["relativeListItem1","relativeListItem2", "relativeListItem3", "relativeListItem4", "relativeListItem2"];
 class App extends Component {
     render() {
-
+        const listItems = relativeList.map((item) =>
+        // Correct! Key should be specified inside the array.
+        <li key={item.toString()}>{item}</li>
+      );
         return(
             <div id="main">
                <ol key={"relativeList"}>
-                <li key={"relativeListItem1"}>Relative 1</li>
-                <li key={"relativeListItem2"}>Relative 2</li>
-                <li key={"relativeListItem3"}>Relative 3</li>
+                   {listItems}
                </ol>
             </div>
         )

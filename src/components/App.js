3f1ff652b2c8a69d01a344ae0cc7158bd8fcda3e
amirbehesti <amirbehesti392@gmail.com>
2022-07-28 17:31:40 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,11 @@ class App extends Component {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <ol key={relativeList}>
+                <li key={relativeListItem1}>Relative 1</li>
+                <li key={relativeListItem2}>Relative 2</li>
+                <li key={relativeListItem3}>Relative 3</li>
+               </ol>
             </div>
         )
     }
